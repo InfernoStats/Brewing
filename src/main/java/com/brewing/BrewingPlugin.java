821bd6e6a036f1a26b2c302a1fc1d1522d4ea972
Fjara <robuhde@gmail.com>
2023-04-03 15:53:07 -0400

    Corrected file paths (#6)
    Correct the resource paths for the infobox images

--- src/main/java/com/brewing/BrewingPlugin.java
+++ src/main/java/com/brewing/BrewingPlugin.java
@@ -49,9 +49,9 @@ public class BrewingPlugin extends Plugin {
 
 	private boolean infoboxInit = false;
 
-	private static final BufferedImage VAT_IMAGE = ImageUtil.loadImageResource(BrewingPlugin.class, "./vat.png");
-	private static final BufferedImage BARREL_IMAGE = ImageUtil.loadImageResource(BrewingPlugin.class, "./barrel.png");
-	private static final BufferedImage THE_STUFF_VAT_IMAGE = ImageUtil.loadImageResource(BrewingPlugin.class, "./vat_stuff.png");
+	private static final BufferedImage VAT_IMAGE = ImageUtil.loadImageResource(BrewingPlugin.class, "/com/brewing/vat.png");
+	private static final BufferedImage BARREL_IMAGE = ImageUtil.loadImageResource(BrewingPlugin.class, "/com/brewing/barrel.png");
+	private static final BufferedImage THE_STUFF_VAT_IMAGE = ImageUtil.loadImageResource(BrewingPlugin.class, "/com/brewing/vat_stuff.png");
 
 	@Provides
 	BrewingConfig provideConfig(ConfigManager configManager) {
